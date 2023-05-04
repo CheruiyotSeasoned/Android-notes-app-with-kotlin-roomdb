d4986c0a85de04317067533f2be7262e98c41b9a
CheruiyotSeasoned <briancheruiyot00@gmail>
2023-05-04 18:26:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notesdan/NoteRepository.kt
@@ -0,0 +1,29 @@
+package com.example.notesdan
+import androidx.lifecycle.LiveData
+import com.example.notesdan.dao.NotesDao
+import com.example.notesdan.entity.Note
+
+class NoteRepository(private val notesDao: NotesDao) {
+
+    // on below line we are creating a variable for our list
+    // and we are getting all the notes from our DAO class.
+    val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
+
+    // on below line we are creating an insert method
+    // for adding the note to our database.
+    suspend fun insert(note: Note) {
+        notesDao.insert(note)
+    }
+
+    // on below line we are creating a delete method
+    // for deleting our note from database.
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+
+    // on below line we are creating a update method for
+    // updating our note from database.
+    suspend fun update(note: Note){
+        notesDao.update(note)
+    }
+}
---
+++ app/src/main/java/com/example/notesdan/viewmodel/ImagesViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.notesdan.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ImagesViewModel(application:Application): AndroidViewModel(application) {
+
+
+
+
+}
